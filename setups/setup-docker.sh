2972361de4f59034a784ad89c72a7e575b7eea86
Hyun <hyun@balancehero.com>
2021-09-04 00:58:09 +0900

    docker study

--- setups/setup-docker.sh
+++ setups/setup-docker.sh
@@ -1,5 +1,5 @@
 sudo yum update -y
 sudo yum install docker -y
 sudo service docker start
-systemctl enable docker.service
+# sudo systemctl enable docker.service #todo 이거 필요한지?
 # TODO consider allow ec2-user to use docker without sudo
--- study/dockerhub.md
+++ study/dockerhub.md
@@ -4,3 +4,11 @@
 - maven처럼 repository역할
 - jenkins처럼 빌드해서 이미지를 생성함
 - 우리는 생성된 이미지를 다운 받아서 실행만 시키면 됨.
+
+
+### login
+private repository를 사용하기 위해선 로그인 해야함.
+```shell
+docker login
+
+```
