e13a1fafd3ed54c21576bd43c891326f6d9b9429
hyun.kim <dss99911@gmail.com>
2022-08-18 02:31:18 +0900

    update

--- setups/setup-docker.sh
+++ setups/setup-docker.sh
@@ -1,5 +1,5 @@
 sudo yum update -y
 sudo yum install docker -y
-sudo service docker start
-# sudo systemctl enable docker.service #todo 이거 필요한지?
+sudo systemctl start docker.service
+# sudo systemctl enable docker.service #todo 이거 필요한지? test server에서는 호출 안함. 잘 작동하면 없어도 될 듯
 # TODO consider allow ec2-user to use docker without sudo
